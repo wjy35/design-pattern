e2b2e5c9da9ff13e17d6c5ccf9b3cb666deaadc8
왕준영 <wangsun7@naver.com>
2023-12-03 11:29:45 +0900

    Feat: GameProgress v1 추가

--- pattern/behavioral/template/v1/GameProgress.java
+++ pattern/behavioral/template/v1/GameProgress.java
@@ -1,2 +1,10 @@
-package behavioral.template.v1;public interface GameProgress {
+package behavioral.template.v1;
+
+public interface GameProgress {
+    void play();
+
+    void waitMember();
+    void round(String member);
+    void calculateGrade();
+    void reflectRank();
 }
